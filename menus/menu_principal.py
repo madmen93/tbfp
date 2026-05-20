b981e1315a10b2bfc46104ac2df3a80928151806
fcs-luka <fabian.cs.13.08@gmail.com>
2026-05-20 13:56:48 -0500

    Se adaptó para uso en sistema unificado

--- menus/menu_principal.py
+++ menus/menu_principal.py
@@ -1,6 +1,26 @@
 from services.flujo_pacientes import menu_flujo_pacientes
 from services.gestion_protocolos import generar_protocolo
+from services.kpi_operativo import mostrar_kpi
+from services.grafico_kpi import mostrar_diagrama_pareto
 from menus.menu_citas import menu_citas
+
+
+def menu_indicadores():
+    while True:
+        print("\n==== INDICADORES DE GESTIÓN ====")
+        print("(1) Ver KPI operativo")
+        print("(2) Abrir diagrama de Pareto")
+        print("(3) Volver")
+
+        opcion = input("\nSeleccione: ")
+        if opcion == "1":
+            print(mostrar_kpi())
+        elif opcion == "2":
+            mostrar_diagrama_pareto()
+        elif opcion == "3":
+            break
+        else:
+            print("Opción inválida.")
 
 
 def menu_principal():
@@ -11,7 +31,7 @@ def menu_principal():
         print("(3) Flujo de pacientes")
         print("(4) Indicadores de gestión")
         print("(5) Salir")
-        
+
         opcion = input("\nEscoja la opción a realizar en esta oportunidad: ")
 
         if opcion == "1":
@@ -21,9 +41,8 @@ def menu_principal():
         elif opcion == "3":
             menu_flujo_pacientes()
         elif opcion == "4":
-            print("4")
+            menu_indicadores()
         elif opcion == "5":
             break
         else:
             print("\nOpción inválida. Intente nuevamente")
-
