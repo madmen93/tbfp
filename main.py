9b95b44f7d06de063fe7848dd40536937aaa28b3
fcs-luka <fabian.cs.13.08@gmail.com>
2026-05-20 13:44:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-#from menus.menu_principal import menu_principal
-from interfaces.app_tkinter import iniciar_app
+import sys
 
-iniciar_app()
-#menu_principal()
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        mode = sys.argv[1].lower()
+        if mode == "cli":
+            from menus.menu_principal import menu_principal
+            menu_principal()
+        elif mode == "simple":
+            from ui_simple import launch_simple_app
+            launch_simple_app()
+        else:
+            print("Modo desconocido. Use 'cli' o 'simple'.")
+    else:
+        from ui_simple import launch_simple_app
+        launch_simple_app()
